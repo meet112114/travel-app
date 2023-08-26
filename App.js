b3282876636bf12462fa62e6ea226b81e0992c4f
meet <mrsanwadkar@gmail.com>
2023-08-26 11:38:15 +0530

    stack nav completed

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView } from 'react-native';
+import SafeView from './src/components/SafeView';
+import StackNavigation from './src/components/Navigation/StackNavigation';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={SafeView.AndroidSafeArea}>
+       <StackNavigation/>
+    </SafeAreaView>
+   
   );
 }
 
---
+++ src/components/screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text , SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <SafeAreaView style={{flex:1,backgroundColor:"while",alignItems:"center"}}>
+       <Text> home</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
